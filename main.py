7080cf768880af445db42776523c719252e809d1
Emanuel Zapata <emanuelzapata@ymail.com>
2021-05-12 20:20:45 -0500

    added a sqotify query with token (token expires after an hr)

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import billboard
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+
+spotifyURL = "https://api.spotify.com/v1/"
+chart = billboard.ChartData('hot-100')
 
 app = Flask(__name__)
 
-chart = billboard.ChartData('hot-100')
-
 @app.route('/')
 def index():
-    return "run-jam"
+    url = spotifyURL+'search/'
+    headers = {'Authorization':'Bearer BQDW4TGVeIYTuql1wJPI2iXOqK-Fg4ttQcZktbAYxKwI34sizaqZZ2h_FeaSOA96aWH2M95X9gYfbcfeXQNkFAJ5WuVk7SG-AdM4erSpcd5sVDJ3DvYwba-2PKeW_19qOmrHKS99-eafkv8jzuTt6yAxRZu0JFB5QGeDrlO17jBzFHq8nkvy6PpxmYzY7D1SEYgqeH62SdSBp7t_x38Gbfc_h4BgWSfqRm-9KZjr5--L2kfxNqzqVl4lpzOLuenpZrpROjlOlNS4uDLfwUVb'}
+    params = {'q':'taylor swift enchanted','type':'track','limit':'1'}
+    response = requests.get(url, params=params, headers=headers)
+    temp = response.json()
+    #print(temp["tracks"]["items"]["id"])
+    #this will get the first track result which should be the proper track if searching by artist AND song name
+    print(temp['tracks']['items'][0]['id'])
+    return response.json()
 
 @app.route('/hot-100')
 def return_hot_100():
