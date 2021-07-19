399d26b3bbfe56555dd0374c36804fe3d1ed6a86
Emanuel Zapata <emanuelzapata@ymail.com>
2021-07-18 22:43:24 -0500

    added pulling the ID's from spotify using the user bearer token

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ app = Flask(__name__)
 
 #helper functions
 def get_track_id(q:str, token:str):
-    return q + " " +  token
+    url = spotify_URL+'search/'
+    params = {'q':q,'type':'track','limit':'1'}
+    headers = {'Authorization':'Bearer {0}'.format(token)}
+    response = requests.get(url,params=params,headers=headers)
+    temp = response.json()
+    try:
+        return temp['tracks']['items'][0]['id']
+    except:
+        return None
+
+def get_track_features(id:str, token:str):
+    return "track features"
 
 def get_header(key:str)->str:
     value = request.headers[key]
@@ -23,7 +34,29 @@ def get_chart_data(chart_name:str):
     local_chart_data = billboard.ChartData(chart_name)
     return local_chart_data
 
+def temp_ids():
+    return "IDS"
+
 #endpoints
+@app.route('/get-track-ids')
+def get_track_IDs():
+    #get all headers using helper
+    chart_data_with_ids = [{}]
+    #bearer_token = get_header('token')
+    chart = get_header('chart')
+    bpm = get_header('bpm')
+    #get chart data  using helper
+    chart_data = get_chart_data(chart)
+    for song in chart_data:
+        temp_song = {}
+        temp_song['song'] = song.title
+        temp_song['artist'] = song.artist
+        temp_song['trackID'] = get_track_id(song.title, config['bearer_token'])
+        chart_data_with_ids.append(temp_song)
+        #print(get_track_id(song.title, config['bearer_token']))#,bearer_token))
+    print(chart_data_with_ids)
+    return "emanuelzapata"
+
 @app.route('/get-tracks')
 def get_tracks():
     #get all headers using helper
