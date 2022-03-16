eac7b1db06558346ba5e9083064bde6337a5123d
Emanuel Zapata <emanuelzapata@ymail.com>
2022-03-15 20:16:30 -0500

    committing asysnc sample

---
+++ Scripts/asyncrequest.py
@@ -0,0 +1,32 @@
+from urllib import request
+import aiohttp
+import asyncio
+import time
+import requests
+
+start_time = time.time()
+
+def nonasync():
+    for number in range(1, 151):
+        pokemon_url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+        response = requests.get(pokemon_url)
+        pokemon = response.json()
+        print(pokemon['name'])
+nonasync()
+print("--- %s seconds ---" % (time.time() - start_time))
+
+start_time = time.time()
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+
+        for number in range(1, 151):
+            pokemon_url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+            async with session.get(pokemon_url) as resp:
+                pokemon = await resp.json()
+                print(pokemon['name'])
+
+asyncio.run(main())
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
