6d0f1f539e3cff12334edd5d69a3dab03c0cd271
Emanuel Zapata <emanuelzapata@ymail.com>
2022-03-20 17:38:44 -0500

    updating requirements

---
+++ Scripts/token.py
@@ -0,0 +1,16 @@
+from dotenv import main
+import os
+main.load_dotenv()
+print(os.getenv('CLIENT_ID'))
+
+# settings.py
+import os
+from os.path import join, dirname
+# from dotenv import load_dotenv
+import dotenv
+dv = dotenv()
+dotenv_path = join(dirname(__file__), '.env')
+dv.load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ.get("CLIENT_ID")
+print(SECRET_KEY)
